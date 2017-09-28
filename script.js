34eff9a1f7a786ca10a82eb777d027fffebd0bfd
Adrià Fontcuberta <adria@calidae.com>
2017-09-28 17:33:51 +0200

    Update script to show that load times are not execution times

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hi from script.js')
+console.log('outside')
+
+/**
+ * Estem parlant dels temps de càrrega i parseig dels arxius, no d'execució.
+ * Aquest timeout no canviarà ni el DOMContentLoaded ni el Load
+ */
+setTimeout(() => {
+  console.log('inside')
+}, 5000)
